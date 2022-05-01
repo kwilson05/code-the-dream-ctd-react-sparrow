3335a0b9b937b6b0bf3d8e2cf8f7231eed89a815
Kasozi Wilson <soziandwelewilson@gmail.com>
2022-05-01 15:52:50 -0400

    useRef exercises

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,13 +1,27 @@
+import {useRef, useEffect} from "react";
+
 const InputWithLabel = ({id, type="text", value, handleTitleChange, children, isFocused }) => {
+
+
+    const inputRef = useRef();
+
+    useEffect( () => {
+        if(isFocused && inputRef.current){
+            inputRef.current.focus();
+        }
+    });
+
+
    return  (<>
             <label htmlFor={id}>
                 {children}
             </label>
             <input 
+            ref={inputRef}
             type={type}
             value={value} 
             onChange={handleTitleChange} 
-            autoFocus={isFocused}
+            
             id={id}/>
             </>);
 };
